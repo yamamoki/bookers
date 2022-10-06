79b3d4fa98c069ab5770ceab665adedc665bfe9d
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-10-06 08:39:46 +0000

    [add]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,29 @@
 class BooksController < ApplicationController
-  def new
+  #def new
+    #@book=Book.new
+  #end
+  
+  def create
+    book=Book.new(book_params)
+    book.save
+    rediect_to'/show'
   end
 
+
   def index
+    @books=Book.all#これで投稿したのが出る
+   
   end
 
   def show
   end
 
   def edit
+  end
+  
+ private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
