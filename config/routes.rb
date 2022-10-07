bb6c54677b92559ac0e2affa3a37bb24c6854d1a
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-10-07 01:44:35 +0000

    [add]Bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  #get 'books/new'
   get 'books/index'#indexがshowになる為残している
+  get '/' => 'homes#top'
   get '/show' =>'books#show'
   #get 'books/edit'
   resources :books
-  get '/' => 'homes#top'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
